2a47e30f9d914e250a9e1aaaf062b6780a50df3c
Laurentiu Udrea <ulaurentiu@gmail.com>
2025-07-03 22:06:43 +0300

    add collision

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.y = y
+        self.x = x
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.x, self.y), self.radius, 2)
+
+    def update(self, dt):
+        self.velocity += (self.velocity * dt)
+
